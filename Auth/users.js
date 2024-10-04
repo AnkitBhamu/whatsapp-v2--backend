9ac28b2709e46f0c2e00fd1347dfd8a2d75243c0
Ankit Kumar <ankit.kumar@dharmalifelabs.com>
2024-10-05 02:47:30 +0530

    fixed some small bugs

--- Auth/users.js
+++ Auth/users.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const db_client = require("../db_client");
+const jwt = require("jsonwebtoken");
 
 let router = express.Router();
+
+function generateWebToken(payload) {
+  let token = jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: "7d",
+  });
+
+  return token;
+}
 
 // for registering the user
 router.post("/user/register", (req, res) => {
@@ -9,17 +18,32 @@ router.post("/user/register", (req, res) => {
   let mobile = req.body.mobile;
   let email = req.body.email;
   let password = req.body.password;
-  let about = req.body.about;
+  let profile_pic = req.body.profile_pic;
+  let country_code = req.body.country_code;
 
   console.log("Request body is : ", req.body);
 
-  let query = `INSERT INTO users (name , mobile , email , password , about) values('${name}','${mobile}','${email}','${password}', '${about}')`;
+  let query = `INSERT INTO users (name , mobile , email , password ,profile_pic,country_code) values('${name}','${mobile}','${email}','${password}', '${profile_pic}','${country_code}')`;
   console.log("Query is : ", query);
 
   db_client
     .query(query)
-    .then((resp) => res.json("Success").status(200))
-    .catch((err) => res.json(err).status(500));
+    .then((resp) => {
+      let token = generateWebToken(req.body);
+      let response_data = {
+        name: name,
+        mobile: mobile,
+        email: email,
+        profile_pic: profile_pic,
+        country_code: country_code,
+        token: token,
+      };
+      res.status(200).json(response_data);
+    })
+    .catch((err) => {
+      console.log("Error came!!");
+      res.status(500).json(err);
+    });
 });
 
 // for loging in the user
@@ -33,9 +57,10 @@ router.get("/user", (req, res) => {
   db_client
     .query(query)
     .then((result) => {
-      res.json(result.rows[0]).status(200);
+      res.status(200).json(result.rows[0]);
     })
-    .catch((err) => res.json(err).status(500));
+    .catch((err) => res.status(500))
+    .json(err);
 });
 
 // for getting all the contacts of the user
@@ -46,8 +71,8 @@ router.get("/user/contacts", (req, res) => {
 
   db_client
     .query(query)
-    .then((result) => res.json(result.rows[0]).status(200))
-    .catch((err) => res.json(err).status(500));
+    .then((result) => res.status(200).json(result.rows[0]))
+    .catch((err) => res.status(500).json(err));
 });
 
 // for fetching a contact name, mobile and about and profile pic
@@ -59,8 +84,8 @@ router.get("/user/details", (req, res) => {
 
   db_client
     .query(query)
-    .then((result) => res.json(result.rows[0]).status(200))
-    .catch((err) => res.json(err).status(500));
+    .then((result) => res.status(200).json(result.rows[0]))
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
